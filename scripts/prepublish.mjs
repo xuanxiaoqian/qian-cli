d81a4dfa4ef8757d47230b3f4f75722c6b5e2d1e
轩小浅 <3102535247@qq.com>
2022-09-02 15:36:51 +0800

    版本号: 1.3.7

--- scripts/prepublish.mjs
+++ scripts/prepublish.mjs
@@ -4,6 +4,17 @@ import "zx/globals";
 await $`npm run build:cli`;
 
 let { version } = JSON.parse(await fs.readFile("./package.json"));
+let _data = JSON.parse(fs.readFileSync("./package.json"));
+
+let v = _data.version.split(".").map(Number);
+
+v[v.length - 1] += 1;
+
+_data.version = v.join(".");
+
+fs.writeFileSync("./package.json", JSON.stringify(_data));
+
+console.log(`版本号： ${version} -> ${_data.version}`);
 
 await $`git add .`;
 

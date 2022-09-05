3648999c749a567c893cd5caf6cd4e14cf63cabf
轩小浅 <3102535247@qq.com>
2022-09-05 14:10:42 +0800

    版本号: 1.3.13

--- src/template/base/vite.config.ts
+++ src/template/base/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig, loadEnv } from "vite";
-import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import { resolve } from "path";
 
@@ -7,7 +7,7 @@ const config = loadEnv("development", "./");
 
 export default defineConfig({
   plugins: [
-    vue(),
+    vueJsx(),
     AutoImport({
       imports: ["vue", "vue-router"],
       dts: "types/auto-import.d.ts",

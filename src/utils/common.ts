c0296cd6047d8f0cee74a2a90a3e5802655db862
轩小浅 <3102535247@qq.com>
2022-09-09 11:07:53 +0800

    优化创建子路由模块算法代码

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,40 +1,39 @@
-import fs from "fs";
+import fs from 'fs'
 
 // 将json数据的属性排一下序
 export default function sortDependencies(packageJson) {
-  const sorted: any = {};
+  const sorted: any = {}
 
   const depTypes = [
-    "dependencies",
-    "devDependencies",
-    "peerDependencies",
-    "optionalDependencies",
-  ];
+    'dependencies',
+    'devDependencies',
+    'peerDependencies',
+    'optionalDependencies',
+  ]
 
   for (const depType of depTypes) {
     if (packageJson[depType]) {
-      sorted[depType] = {};
+      sorted[depType] = {}
 
       Object.keys(packageJson[depType])
         .sort()
         .forEach((name) => {
-          sorted[depType][name] = packageJson[depType][name];
-        });
+          sorted[depType][name] = packageJson[depType][name]
+        })
     }
   }
 
   return {
     ...packageJson,
     ...sorted,
-  };
+  }
 }
 
 // 判断是否是object
-const isObject = (val) => val && typeof val === "object";
+const isObject = (val) => val && typeof val === 'object'
 
 // 数组去重
-const mergeArrayWithDedupe = (a, b) =>
-  Array.from(new Set([...a, ...b]));
+const mergeArrayWithDedupe = (a, b) => Array.from(new Set([...a, ...b]))
 
 /**
  * Recursively merge the content of the new object to the existing one
@@ -43,38 +42,110 @@ const mergeArrayWithDedupe = (a, b) =>
  */
 export function deepMerge(target, obj) {
   for (const key of Object.keys(obj)) {
-    const oldVal = target[key];
-    const newVal = obj[key];
+    const oldVal = target[key]
+    const newVal = obj[key]
 
     if (Array.isArray(oldVal) && Array.isArray(newVal)) {
-      target[key] = mergeArrayWithDedupe(oldVal, newVal);
+      target[key] = mergeArrayWithDedupe(oldVal, newVal)
     } else if (isObject(oldVal) && isObject(newVal)) {
-      target[key] = deepMerge(oldVal, newVal);
+      target[key] = deepMerge(oldVal, newVal)
     } else {
-      target[key] = newVal;
+      target[key] = newVal
     }
   }
 
-  return target;
+  return target
 }
-
-
 
 /**
  * 读取指定路径下 json 文件
  * @param filename json 文件的路径
  */
- export function readJsonFile<T>(filename: string): T {
-  return JSON.parse(fs.readFileSync(filename, { encoding: "utf-8", flag: "r" }));
+export function readJsonFile<T>(filename: string): T {
+  return JSON.parse(fs.readFileSync(filename, { encoding: 'utf-8', flag: 'r' }))
 }
 
-
+/**
+ *
+ * @param str 将字符串首字母大写
+ * @returns
+ */
+export function firstToUpper(str: string) {
+  return str.trim().toLowerCase().replace(str[0], str[0].toUpperCase())
+}
 
 /**
- * 
- * @param str 将字符串首字母大写
- * @returns 
+ * 传进来一个模块，返回模块里面默认导出的名字
+ * @param stringValue 要转换的字符串
+ * @returns 默认导出名
  */
- export function firstToUpper(str:string) {
-  return str.trim().toLowerCase().replace(str[0], str[0].toUpperCase());
+export function exportDefaultName(stringValue: string) {
+  let matchString = stringValue.match(/export+\s+default+\s+\w+/)
+
+  if (!matchString) return false
+
+  let name = matchString[0].replace(/\s+/g, ' ')
+  let exportDefaultName = name.split(' ')[2]
+  return exportDefaultName
+}
+
+/**
+ * 传进来一个模块、一个对象名，返回一个可以调用的对象
+ * @param objectString 模块
+ * @param name 对象名
+ * @returns 动态对象
+ */
+export function stringObjectParse(objectString: string, name: string) {
+  // 这里是为了防止对象里面有跟name重名的字段
+  let splitStringObject: any = objectString.split(name)
+  splitStringObject.shift()
+  splitStringObject = splitStringObject.join(name)
+
+  let flag = 0 //记录括号是否闭合
+  let latestIndex = 0
+  let EndIndex = 0
+
+  for (let i = 0; i < splitStringObject.length; i++) {
+    if (splitStringObject[i] == '{') {
+      if (flag == 0) {
+        latestIndex = i
+      }
+      flag += 1
+    }
+
+    if (splitStringObject[i] == '}') {
+      flag -= 1
+    }
+
+    if (flag == 0 && latestIndex !== 0) {
+      EndIndex = i
+      break
+    }
+  }
+
+  let obj = new Function(
+    'value',
+    `
+        return eval("(" + value + ")");
+      `,
+  ).call(this, splitStringObject.substring(latestIndex, EndIndex + 1))
+
+  return obj
+}
+
+/**
+ * 传进来一个js数组变量，一比一的转为字符串数组变量
+ * @param arr 数组变量
+ * @returns 字符串的数组格式
+ */
+export function JSONArrModule(arr: string) {
+  let obj = JSON.stringify(arr, (key, value) => {
+    if (value instanceof Function) {
+      return value.toString()
+    }
+
+    return value
+  })
+
+  return obj
 }
--- src/utils/create.ts
+++ src/utils/create.ts
@@ -1,57 +1,63 @@
-import path from "path";
-import ejs from "ejs";
-import fs from "fs";
-import inquirer from "inquirer";
-import shell from "shelljs";
-import { firstToUpper, readJsonFile } from "./common";
+import path from 'path'
+import ejs from 'ejs'
+import fs from 'fs'
+import inquirer from 'inquirer'
+import shell from 'shelljs'
+import {
+  exportDefaultName,
+  firstToUpper,
+  JSONArrModule,
+  readJsonFile,
+  stringObjectParse,
+} from './common'
 
 export async function selectModule(): Promise<string> {
   const question = [
     {
-      type: "list",
-      message: "请选择要创建什么模块",
-      name: "module",
-      choices: ["router", "store"],
+      type: 'list',
+      message: '请选择要创建什么模块',
+      name: 'module',
+      choices: ['router', 'store'],
     },
-  ];
-  let answer = await inquirer.prompt(question);
+  ]
+  let answer = await inquirer.prompt(question)
 
-  return answer.module as string;
+  return answer.module as string
 }
 
 export function createModule(moduleStr: string, moduleName: string) {
-  if (moduleStr == "router") {
-    createRouter(moduleName);
-  } else if (moduleStr == "store") {
-    createStore(moduleName);
+  if (moduleStr == 'router') {
+    createRouter(moduleName)
+  } else if (moduleStr == 'store') {
+    createStore(moduleName)
   }
 }
 
 export function createRouter(moduleName: string) {
-  if (moduleName.split("/").length == 1) {
-    createRootRouter(moduleName);
-  } else if (moduleName.split("/").length == 2) {
-    createChildren(moduleName);
+  if (moduleName.split('/').length == 1) {
+    createRootRouter(moduleName)
+  } else if (moduleName.split('/').length == 2) {
+    createChildren(moduleName)
   }
 }
 
 export function createRootRouter(moduleName: string) {
-  let _data: any = readJsonFile(path.join(process.cwd(), "qian-cli.json"));
+  let _data: any = readJsonFile(path.join(process.cwd(), 'qian-cli.json'))
 
   // 模版文件目录
-  let templateUrl = path.join(__dirname, "../", "template", "create");
+  let templateUrl = path.join(__dirname, '../', 'template', 'create')
 
-  let renderRouteUrl = path.join(process.cwd(), _data.router.routePath);
-  let renderPageUrl = path.join(process.cwd(), _data.router.pagePath);
+  let renderRouteUrl = path.join(process.cwd(), _data.router.routePath)
+  let renderPageUrl = path.join(process.cwd(), _data.router.pagePath)
 
-  let pageUrl = path.join(templateUrl, "page.ejs");
-  let routeUrl = path.join(templateUrl, "route.ejs");
+  let pageUrl = path.join(templateUrl, 'page.ejs')
+  let routeUrl = path.join(templateUrl, 'route.ejs')
 
-  let tsSuffix = ".ts";
-  let routeName = moduleName + tsSuffix;
+  let tsSuffix = '.ts'
+  let routeName = moduleName + tsSuffix
 
-  let vueSuffix = ".vue";
-  let vueName = moduleName + vueSuffix;
+  let vueSuffix = '.vue'
+  let vueName = moduleName + vueSuffix
 
   let routerMeta = `
     path: '/${moduleName}',
@@ -59,13 +65,13 @@ export function createRootRouter(moduleName: string) {
     component: () => import('${path
       .join(
         _data.router.alias,
-        _data.router.pagePath.split("src")[1],
-        _data.router.isPageDir ? moduleName : "",
-        moduleName + ".vue"
+        _data.router.pagePath.split('src')[1],
+        _data.router.isPageDir ? moduleName : '',
+        moduleName + '.vue',
       )
-      .split("\\")
-      .join("/")}'),
-  `;
+      .split('\\')
+      .join('/')}'),
+  `
 
   ejs
     .renderFile(pageUrl, {
@@ -73,181 +79,143 @@ export function createRootRouter(moduleName: string) {
     })
     .then((data) => {
       if (_data.router.isPageDir) {
-        fs.mkdirSync(path.join(renderPageUrl, moduleName));
-        fs.writeFileSync(path.join(renderPageUrl, moduleName, vueName), data);
+        fs.mkdirSync(path.join(renderPageUrl, moduleName))
+        fs.writeFileSync(path.join(renderPageUrl, moduleName, vueName), data)
       } else {
-        fs.writeFileSync(path.join(renderPageUrl, vueName), data);
+        fs.writeFileSync(path.join(renderPageUrl, vueName), data)
       }
-    });
+    })
 
   ejs
     .renderFile(routeUrl, {
-      routeName: moduleName,
+      routeName: moduleName + 'Route',
       routerMeta: routerMeta,
-      children: "[]",
+      children: '[]',
     })
     .then((data) => {
       // 生成 ejs 处理后的模版文件
-      fs.writeFileSync(path.join(renderRouteUrl, routeName), data);
+      fs.writeFileSync(path.join(renderRouteUrl, routeName), data)
 
       if (
         shell.exec(
-          `npx prettier --write  \"src/router/modules/${moduleName}.ts\"`
+          `npx prettier --write  \"src/router/modules/${moduleName}.ts\"`,
         ).code !== 0
       ) {
-        shell.echo("prettier格式化错误");
+        shell.echo('prettier格式化错误')
       }
-    });
+    })
 }
 
 export function createChildren(moduleName: string) {
-  let _data: any = readJsonFile(path.join(process.cwd(), "qian-cli.json"));
-  let templateUrl = path.join(__dirname, "../", "template", "create");
+  let _data: any = readJsonFile(path.join(process.cwd(), 'qian-cli.json'))
+  let templateUrl = path.join(__dirname, '../', 'template', 'create')
 
-  let fatherName = moduleName.split("/")[0];
-  let sonName = moduleName.split("/")[1];
+  let fatherName = moduleName.split('/')[0]
+  let sonName = moduleName.split('/')[1]
 
   let fatherUrl = path.join(
     process.cwd(),
     _data.router.routePath,
-    fatherName + ".ts"
-  );
+    fatherName + '.ts',
+  )
 
-  let route = fs.readFileSync(fatherUrl).toString();
-  let chidren = {
+  let route = fs.readFileSync(fatherUrl).toString()
+  let children = {
     path: `${sonName}`,
     name: `${sonName}`,
     component: _data.router.isPageDir
       ? `() => import("@/views/${fatherName}/${sonName}/${sonName}.vue")`
       : `() => import("@/views/${sonName}.vue")`,
-  };
-  let splitRouteObject = route.split(`${fatherName}Route:`)[1];
-  let flag = 0; //记录括号是否闭合
-  let latestIndex = 0;
-  let EndIndex = 0;
-
-  for (let i = 0; i < splitRouteObject.length; i++) {
-    if (splitRouteObject[i] == "{") {
-      if (flag == 0) {
-        latestIndex = i;
-      }
-      flag += 1;
-    }
-
-    if (splitRouteObject[i] == "}") {
-      flag -= 1;
-    }
-
-    if (flag == 0 && latestIndex !== 0) {
-      EndIndex = i;
-      break;
-    }
   }
 
-  // var obj = eval(
-  //   "(" + splitRouteObject.substring(latestIndex, EndIndex + 1) + ")"
-  // );
+  let defaultName = exportDefaultName(route) as string
+  let obj = stringObjectParse(route, defaultName)
 
-  let obj = new Function(
-    "value",
-    `
-    return eval("(" + value + ")");
-  `
-  ).call(this, splitRouteObject.substring(latestIndex, EndIndex + 1));
+  let chid = obj.children ?? []
+  chid.push(children)
+  obj.children = chid
 
-  let chid = obj.children ?? [];
-  chid.push(chidren);
-  obj.children = chid;
-
-  let arr = obj.children;
-
-  let data = JSON.stringify(arr, (key, value) => {
-    if (value instanceof Function) {
-      return value.toString();
-    }
-
-    return value;
-  })
+  let data = JSONArrModule(chid)
     .replace(/\\"/g, '"')
-    .replace(/"([^"]+)":/g, "$1:")
-    .replace(/"\(/g, "(")
-    .replace(/\)"/g, ")");
+    .replace(/"([^"]+)":/g, '$1:')
+    .replace(/"\(/g, '(')
+    .replace(/\)"/g, ')')
 
-  let result = "";
+  let result = ''
   for (let key in obj) {
-    if (key == "children") {
+    if (key == 'children') {
     } else if (obj[key] instanceof Object) {
       let s = JSON.stringify(obj[key], (key, value) => {
         if (value instanceof Function) {
-          return value.toString();
+          return value.toString()
         }
 
-        return value;
+        return value
       })
         .replace(/\\"/g, '"')
-        .replace(/"([^"]+)":/g, "$1:")
-        .replace(/"\(/g, "(")
-        .replace(/\)"/g, ")");
+        .replace(/"([^"]+)":/g, '$1:')
+        .replace(/"\(/g, '(')
+        .replace(/\)"/g, ')')
 
-      result += `${key}: ${s},`;
+      result += `${key}: ${s},`
     } else {
-      result += `${key}: "${obj[key]}",`;
+      result += `${key}: "${obj[key]}",`
     }
   }
 
   // 一定要渲染page再渲染路由，这个bug让我去看了几天vite源码企图修改源码来解决
   // 如果渲染route再渲染page会报vite的[plugin:vite:import-analysis] Cannot read properties of undefined (reading 'url')错
-  let renderPageUrl = path.join(process.cwd(), _data.router.pagePath);
+  let renderPageUrl = path.join(process.cwd(), _data.router.pagePath)
   ejs
-    .renderFile(path.join(templateUrl, "page.ejs"), {
+    .renderFile(path.join(templateUrl, 'page.ejs'), {
       pageName: sonName,
     })
     .then((data) => {
       if (_data.router.isPageDir) {
-        fs.mkdirSync(path.join(renderPageUrl, fatherName, sonName));
+        fs.mkdirSync(path.join(renderPageUrl, fatherName, sonName))
         fs.writeFileSync(
-          path.join(renderPageUrl, fatherName, sonName, sonName + ".vue"),
-          data
-        );
+          path.join(renderPageUrl, fatherName, sonName, sonName + '.vue'),
+          data,
+        )
       } else {
-        fs.writeFileSync(path.join(renderPageUrl, sonName + ".vue"), data);
+        fs.writeFileSync(path.join(renderPageUrl, sonName + '.vue'), data)
       }
-    });
+    })
 
   ejs
-    .renderFile(path.join(templateUrl, "route.ejs"), {
-      routeName: fatherName,
+    .renderFile(path.join(templateUrl, 'route.ejs'), {
+      routeName: defaultName,
       routerMeta: result,
       children: data,
     })
     .then((data) => {
-      fs.writeFileSync(fatherUrl, data);
+      fs.writeFileSync(fatherUrl, data)
 
       if (
         shell.exec(
-          `npx prettier --write  \"src/router/modules/${fatherName}.ts\"`
+          `npx prettier --write  \"src/router/modules/${fatherName}.ts\"`,
         ).code !== 0
       ) {
-        shell.echo("prettier格式化错误");
+        shell.echo('prettier格式化错误')
       }
-    });
+    })
 }
 
 export function createStore(moduleName: string) {
-  let _data: any = readJsonFile(path.join(process.cwd(), "qian-cli.json"));
+  let _data: any = readJsonFile(path.join(process.cwd(), 'qian-cli.json'))
 
   // 模版文件目录
-  let templateUrl = path.join(__dirname, "../", "template", "create");
+  let templateUrl = path.join(__dirname, '../', 'template', 'create')
 
-  let renderStorerl = path.join(process.cwd(), _data.store.storePath);
+  let renderStorerl = path.join(process.cwd(), _data.store.storePath)
 
   ejs
-    .renderFile(path.join(templateUrl, "store.ejs"), {
+    .renderFile(path.join(templateUrl, 'store.ejs'), {
       storeName: firstToUpper(moduleName),
       storeId: moduleName,
     })
     .then((data) => {
       // 生成 ejs 处理后的模版文件
-      fs.writeFileSync(path.join(renderStorerl, moduleName + ".ts"), data);
-    });
+      fs.writeFileSync(path.join(renderStorerl, moduleName + '.ts'), data)
+    })
 }

9f6cb13047f5ac229dc0858a1392179dd3754163
轩小浅 <3102535247@qq.com>
2022-09-23 16:47:59 +0800

    版本号: 1.3.32

--- src/template/code/axios/src/utils/http/defaultConfig.ts
+++ src/template/code/axios/src/utils/http/defaultConfig.ts
@@ -4,9 +4,8 @@ export const defaultConfig: AxiosRequestConfig = {
   baseURL: import.meta.env.VITE_APP_BASE_API,
   timeout: 10000,
   headers: {
-    tokenValue: localStorage.getItem('tokenValue') ?? '',
+    tokenValue: localStorage.getItem('tokenValue') ?? null,
     'Content-Type': 'application/json;charset=UTF-8',
-    'X-Requested-With': 'XMLHttpRequest'
   }
 }
 

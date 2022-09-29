3f027780deb9780bf28c5c5ced825efe01d4f725
轩小浅 <3102535247@qq.com>
2022-09-29 09:39:15 +0800

    代码优化

--- src/core/create.ts
+++ src/core/create.ts
@@ -9,7 +9,7 @@ export default async function create(moduleName: string): Promise<void> {
 
   let moduleStr
 
-  // 当初不是始化子路由的时候
+  // 当用户初始化子路由的时候不需要用户选择创建什么模块
   if (moduleName.split('/').length == 1) {
     moduleStr = await selectModule()
   }

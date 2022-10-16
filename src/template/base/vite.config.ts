736b06daa6ef5b95b23bae606385193073e79989
xuanxiaoqian <3102535247@qq.com>
2022-10-16 09:13:27 +0800

    版本号: 1.3.40

--- src/template/base/vite.config.ts
+++ src/template/base/vite.config.ts
@@ -1,36 +1,36 @@
-import { defineConfig, loadEnv } from "vite";
-import vue from "@vitejs/plugin-vue";
-import AutoImport from "unplugin-auto-import/vite";
-import { resolve } from "path";
+import { defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import { resolve } from 'path'
 
-const config = loadEnv("development", "./");
+const config = loadEnv('development', './')
 
 export default defineConfig({
   plugins: [
     vue(),
     AutoImport({
-      imports: ["vue", "vue-router"],
-      dts: "types/auto-import.d.ts",
+      imports: ['vue', 'vue-router'],
+      dts: 'types/auto-import.d.ts',
     }),
   ],
 
   server: {
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     port: 3000,
     open: false,
-    base: "./",
+    base: config.VITE_APP_BASE_PATH,
     proxy: {
-      "^/api": {
-        target: config.VITE_APP_BASE_API,
+      '^/api': {
+        target: 'http://localhost:1234',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
 
   resolve: {
     alias: {
-      "@": resolve(__dirname, "./src"),
+      '@': resolve(__dirname, './src'),
     },
   },
-});
+})

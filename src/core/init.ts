3bf011fab74fb5b6bd85bf59a750f42fc0295946
xuanxiaoqian <3102535247@qq.com>
2022-10-19 15:11:14 +0800

    版本号: 1.3.45

--- src/core/init.ts
+++ src/core/init.ts
@@ -14,7 +14,7 @@ export default async function init(name: string, git: string[]): Promise<void> {
   // 用户使用远程git仓库
   if (git.length > 0) {
     var regex = new RegExp(/^http(s)?:\/\/.*\.git$/)
-
+ 
     // 如果不是 http/https开头和.git结尾
     if (!regex.test(git[0])) {
       console.log(red(`无效git地址${git[0]},必须以.git结尾`))
